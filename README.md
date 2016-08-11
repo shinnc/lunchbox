253b1a1d01cd1c80188e74bc01395124e914ab3e
Shinn Chong <shinnyx@gmail.com>
2016-08-11 09:07:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Random Lunchbox generator
+=========================
+
+Based on [Kid Approved Lunch Box + Snack Ideas](http://www.thirtyhandmadedays.com/2014/08/kid-approved-lunch-box-snack-ideas/)
---
+++ lunchbox.rb
@@ -0,0 +1,9 @@
+FRUITS = ['Apples', 'Carrots', 'Oranges', 'Celery', 'Grapes', 'Cucumbers', 'Strawberries', 'Snap peas', 'Bananas', 'Cherry tomatoes', 'Bluberries', 'Sliced peppers']
+
+SNACKS = ['String cheese', 'Chips', 'Yogurt', 'Cheese slices', 'Pretzels', 'Trial mix', 'Crackers', 'Raisins', 'Graham crackers', 'Waffle sticks', 'Granola bar', 'Popcorn']
+
+MAIN = ['Bagel & cream cheese', 'Turkey sandwich', 'Spaghetti in a thermos', 'Meat slices', 'Pita sandwich', 'Chicken salad', 'Peanut butter & jelly', 'Cottage cheese', 'Turkey bites', 'Tacos', 'Homemade pizza rolls', 'Boiled eggs']
+
+MISC = ['Marinara sauce', 'Water bottle', 'Ranch dressing', 'Milk box', 'Hazelnut spread', 'Chocolate milk', 'Peanut butter dip', 'Strawberry milk', 'Juice box', 'Flavored water']
+
+puts "#{FRUITS.sample}, #{SNACKS.sample}, #{MAIN.sample}, #{MISC.sample}"
